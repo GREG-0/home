d6b5b20d03d78492fcc7cefe36cf9c2f969e0ee0
GREG-0 <62487894+GREG-0@users.noreply.github.com>
2021-03-07 20:40:12 +0100

    Update Aktualizacja_Wtyczki_GREGUpdater.sh

--- Aktualizacja_Wtyczki_GREGUpdater.sh
+++ Aktualizacja_Wtyczki_GREGUpdater.sh
@@ -11,7 +11,7 @@ sleep 3
 echo 'Rozpoczynam_Proces...'
 echo ""
 sleep 5
-rm --rf /usr/scripts 
+rm --rf /usr/scripts/*
 rm -f /tmp/GREGUpdater.tar.gz 2>/dev/null
 wget -q -O /tmp/GREGUpdater.tar.gz "http://gregfhd.freeddns.org:2668/share.cgi?ssid=0qAPBWH&fid=0qAPBWH&filename=GREGUpdater.tar.gz&openfolder=forcedownload&GREGUpdater.tar.gz" 2>/dev/null
 
